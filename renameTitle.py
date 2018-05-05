4053ecb08826cf4bea8e6e0d095f717850c4ce5c
Thomas Visentin <tho.visentin@gmail.com>
2018-05-05 15:07:11 +0200

    Press Y for accept all

--- renameTitle.py
+++ renameTitle.py
@@ -1,11 +1,12 @@
 import sys, re, os
 
 pattern = re.compile("([s|S]\d+)?([e|E])?\d+$")
+yesAll = 'n'
 print ("If you want to change your file says yes (y) or no (n): ")
 for arg in sys.argv[1:]:
     toRename = arg
     toSave = ""
-    if arg.find('[') != -1:
+    if arg[0] == '[':
         delete = arg.find(']')
         arg = arg[delete+2:]
     if arg.find('/'):
@@ -28,8 +29,13 @@ for arg in sys.argv[1:]:
         else:
             toSave += string
             toSave += ' '
-    char = input("== ")
-    if (char == 'y' or char == 'Y'):
+    if (yesAll != 'Y'):
+        char = input("== ")
+    if (char == 'y'):
+        os.rename(toRename, toSave)
+        print ("File is changed")
+    elif (char == 'Y'):
+        yesAll = 'Y'
         os.rename(toRename, toSave)
         print ("File is changed")
     else:
